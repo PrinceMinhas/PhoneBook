09601cb654debb26663f7922f00c7c5f0fc87634
Prince Minhas <Prince.Minhas@ibm.com>
2018-05-16 10:35:36 -0400

    Added bootstrap components

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,6 +12,25 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', ['$scope', function($scope) {
   $scope.names = [];
   $scope.addContact = function() {
+    $('#sAlert')[0].style.display = "block";
+    $('#sAlert').delay(5000).fadeOut(2000);
     $scope.names.push({name:$scope.fullName, number:$scope.phoneNumber});
   };
-}]);
+  $scope.currentPage = 0;
+  $scope.pageSize = 10;
+  $scope.numberOfPages=function(){
+    if (Math.ceil($scope.names.length/$scope.pageSize) == 0) {
+      return 1;
+    }
+    else {
+      return Math.ceil($scope.names.length/$scope.pageSize);
+    }                
+  }
+}])
+
+.filter('startFrom', function() {
+  return function(input, start) {
+      start = +start; //parse to int
+      return input.slice(start);
+  }
+});
